ede685777cce1b2d8f99da2488d81d6a6b7f04ee
freeboardtortoise <freeboardtortoise@proton.me>
2026-08-09 19:15:40 +0200

    collisions and more blocks
    added collisions and fixed some collision bugs
    added more blocks to the inventory
    better placing system

--- code.py
+++ code.py
@@ -17,31 +17,126 @@ y=0
 invent = 1
 inventmen = False
 inventbtnPresses = [False, False, False, False]
-inventory = {"grass": 100, "planks": 100, "stone": 100}
-inventoryLayout = [["grass", "planks", "stone","","",""],
+
+inventory = {"grass": 100, "planks": 100, "stone": 100, "leaves": 10, "logs": 2, "chest": 1}
+inventoryLayout = [["grass", "planks", "stone","leaves","logs",""],
+                   ["chest","","","","",""],
                    ["","","","","",""],
                    ["","","","","",""],
                    ["","","","","",""],
                    ["","","","","",""]]
-sprites = {"grass":103, "planks":104, "":150, "stone":105}
-placeSprites = {"grass":2, "planks":1, "stone":3}
+sprites = {"grass":103, "planks":104, "":150, "stone":105, "leaves": 4, "logs": 5, "chest": 18}
+placeSprites = {"grass":2, "planks":1, "stone":3, "": 0, "leaves": 4, "logs": 5, "chest": 18}
 inventorySellection = [0,0]
+cBlock = ""
 
 xcounter = 1
 ycounter = 1
 speed = 1
+walkable_blocks = ["grass"]
+walkable_blocks = [placeSprites[block] for block in walkable_blocks]
 
 
+# placing variables
+placingMode = False
+currentDelta = [0,0]
+currentButtonValues = [False, False, False, False]
+MAX_REACH = 4
+def placing(player_pos):
+ global placingMode, currentDelta, currentButtonValues
+ # 5. Placing blocks (using player's exact world position)
+ if btn(4):  # Place block
+  if placingMode == False:
+   currentDelta = [0,0]
+  placingMode = True
+ if placingMode == True:
+  spr(100, int(player_pos[0]/8)*8 + 8 * currentDelta[0],int(player_pos[1]/8)*8 +  8 * currentDelta[1], colorkey=0)
+  if btn(4) == False:
+   placingMode = False
+   if mget(int(x / 8) + currentDelta[0], int(y / 8) + currentDelta[1]) != placeSprites[cBlock]:
+    if inventory[cBlock] > 0:
+     mset(int(x / 8) + currentDelta[0], int(y / 8) + currentDelta[1], placeSprites[cBlock])
+     inventory[cBlock] -= 1
 
+  if currentButtonValues[0] == True:
+   if btn(0) == False:
+    currentButtonValues[0] = False
+    currentDelta[1] -= 1
+  if currentButtonValues[1] == True:
+   if btn(1) == False:
+    currentButtonValues[1] = False
+    currentDelta[1] += 1
+  if currentButtonValues[2] == True:
+   if btn(2) == False:
+    currentButtonValues[2] = False
+    currentDelta[0] -= 1
+  if currentButtonValues[3] == True:
+   if btn(3) == False:
+    currentButtonValues[3] = False
+    currentDelta[0] += 1
+  if abs(currentDelta[0]) > MAX_REACH:
+   currentDelta[0] += MAX_REACH - currentDelta[0]
+  if abs(currentDelta[1]) > MAX_REACH:
+   currentDelta[1] += MAX_REACH - currentDelta[1]
+
+
+  if btn(0):
+   currentButtonValues[0] = True
+  if btn(1):
+   currentButtonValues[1] = True
+  if btn(2):
+   currentButtonValues[2] = True
+  if btn(3):
+   currentButtonValues[3] = True
+ return placingMode
 
 def TIC():
  global t
  global x
  global y
- global invent, inventmen
+ global invent, inventmen, cBlock, walkable_blocks
  global inventory, inventoryLayout, inventbtnPresses
  global xcounter, ycounter
  global buttonDown, buttonUp, buttonRight, buttonLeft
+ SCREEN_W = 240
+ SCREEN_H = 136
+ HALF_W = SCREEN_W // 2
+ HALF_H = SCREEN_H // 2
+
+# Total map size in pixels
+ map_pixel_w = SCREEN_SIZE[0] * 8
+ map_pixel_h = SCREEN_SIZE[1] * 8
+
+ cls(15)
+
+# 1. Keep player (x, y) strictly within map boundaries
+ if x < 0:
+  x = 0
+ if y < 0:
+  y = 0
+ if x > map_pixel_w:
+  x = map_pixel_w
+ if y > map_pixel_h:
+  y = map_pixel_h
+
+# 2. Calculate camera position centered on the player
+ cam_x = x - HALF_W
+ cam_y = y - HALF_H
+
+# Clamp camera so it never scrolls past the map edges
+ max_cam_x = map_pixel_w - SCREEN_W
+ max_cam_y = map_pixel_h - SCREEN_H
+
+ if cam_x < 0:
+  cam_x = 0
+ if cam_x > max_cam_x:
+  cam_x = max_cam_x
+ if cam_y < 0:
+  cam_y = 0
+ if cam_y > max_cam_y:
+  cam_y = max_cam_y
+
+ map(int((cam_x/speed)/8), int((cam_y/speed)/8), sx=-(cam_x%8), sy=-(cam_y%8))
  if inventmen == True:
   cls(15)
   map(int((x/speed)/8), int((y/speed)/8))
@@ -86,7 +181,7 @@ def TIC():
   if btn(2): inventbtnPresses[2] = True
   if btn(3): inventbtnPresses[3] = True
   if btn(4):
-   invent = placeSprites[inventoryLayout[inventorySellection[1]][inventorySellection[0]]]
+   cBlock = inventoryLayout[inventorySellection[1]][inventorySellection[0]]
    inventmen = False
   if inventbtnPresses[0] == True and btn(0) == False:
    inventorySellection[1] -= 1
@@ -113,28 +208,34 @@ def TIC():
 
   if inventorySellection[1] < 0:
    inventorySellection[1] = len(inventoryLayout) - 1
+ elif placing(( x - cam_x, y - cam_y)) == True:
+  pass
  else:
-  if btn(0): y -= 1
-  if btn(1): y += 1
-  if btn(2): x -= 1
-  if btn(3): x += 1
 
-  cls(15)
-  if x < 0:
-    x = 0
-  if y < 0:
-    y = 0
-  if x > SCREEN_SIZE[0]*8:
-    x = SCREEN_SIZE[0]*8
-  if y > SCREEN_SIZE[1]*8:
-    y = SCREEN_SIZE[1]*8
-  map(int((x/speed)/8),int((y/speed)/8), sx=-(x%8), sy=-(y%8))
-  spr(49, int(240/2), int(136/2))
-  t+=1
+  if btn(0): 
+   if mget(int(x / 8), int((y-1) / 8)) in walkable_blocks and mget(int((x+7) / 8), int((y-1) / 8)) in walkable_blocks:
+    y -= 1
+  if btn(1):
+   if mget(int(x / 8), int((y+8) / 8)) in walkable_blocks and mget(int((x+7) / 8), int((y+8) / 8)) in walkable_blocks:
+    y += 1
+  if btn(2):
+   if mget(int((x-1) / 8), int((y) / 8)) in walkable_blocks and mget(int((x-1) / 8), int((y+7) / 8)) in walkable_blocks:
+    x -= 1
+  if btn(3):
+   if mget(int((x+8) / 8), int((y) / 8)) in walkable_blocks and mget(int((x+8) / 8), int((y+7) / 8)) in walkable_blocks:
+    x += 1
 
-  # placing blocks
-  if btn(4): #place
-   mset(int((240/2)/8)+int((x/speed)/8), int((136/2)/8)+int((y/speed)/8), invent)
- if btn(5):
-  inventmen = True
 
+
+# 3. Render map using the camera coordinates
+
+
+# 4. Render sprite (centered normally, but moves to the edge when near map borders)
+  spr(256, x - cam_x, y - cam_y, colorkey=0)
+  t += 1
+
+
+
+
+  if btn(5):
+   inventmen = True
