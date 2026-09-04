8169aef8ea09c2885a924ad88fd5df12364639fe
freeboardtortoise <freeboardtortoise@proton.me>
2026-09-04 15:58:10 +0200

    re fixed the mob error

--- code.py
+++ code.py
@@ -181,12 +181,11 @@ class Mob:
  
  def pathfinding(self, move):
   pos = self.currentPos + move
-  if mget(int(pos.x / 8), int(pos.y / 8)) not in walkable_blocks or mget(int((pos.x+7) / 8), int((pos.y) / 8)) not in walkable_blocks or mget(int((pos.x) / 8), int((pos.y+7) / 8)) not in walkable_blocks or mget(int((pos.x+7) / 8), int((pos.y+7) / 8)) not in walkable_blocks:
-   # try and move only up and down
-   if  mget(int((self.currentPos.x / 8)), int((pos.y+7) / 8)) not in walkable_blocks or mget(int((self.currentPos.x+7) / 8), int((pos.y+7) / 8)) not in walkable_blocks:
-    move.x = 0
-   if mget(int(pos.x / 8), int((self.currentPos.x) / 8)) not in walkable_blocks or mget(int((pos.x+7) / 8), int((self.currentPos.x) / 8)) not in walkable_blocks : 
-    move.y = 0
+  # try and move only up and down
+  if  mget(int((self.currentPos.x / 8)), int((pos.y+7) / 8)) not in walkable_blocks or mget(int((self.currentPos.x+7) / 8), int((pos.y+7) / 8)) not in walkable_blocks or mget(int((self.currentPos.x / 8)), int((pos.y) / 8)) not in walkable_blocks or mget(int((self.currentPos.x+7) / 8), int((pos.y) / 8)) not in walkable_blocks:
+   move.y = 0
+  if  mget(int((pos.x / 8)), int((self.currentPos.y+7) / 8)) not in walkable_blocks or mget(int((pos.x+7) / 8), int((self.currentPos.y+7) / 8)) not in walkable_blocks or mget(int((pos.x / 8)), int((self.currentPos.y) / 8)) not in walkable_blocks or mget(int((pos.x+7) / 8), int((self.currentPos.y) / 8)) not in walkable_blocks:
+   move.x = 0
 
   # do the actual moving
   self.currentPos +=  move
